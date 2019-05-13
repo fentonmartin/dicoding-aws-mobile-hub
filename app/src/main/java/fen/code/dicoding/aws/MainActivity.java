d41b1138f5e626eb835eeea0bcc2bddf19de9874
Fenton Martin <fentonxm@gmail.com>
2019-05-14 00:37:30 +0700

    Initiate AWSMobileClient

--- app/src/main/java/fen/code/dicoding/aws/MainActivity.java
+++ app/src/main/java/fen/code/dicoding/aws/MainActivity.java
@@ -1,7 +1,11 @@
 package fen.code.dicoding.aws;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /* Initiate AWSMobileClient */
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
     }
 }
